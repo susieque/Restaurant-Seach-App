a360a0e7ee4354b7ec97a53afc704a86d3a1858e
Susie Riley <susie09rob@gmail.com>
2022-01-27 10:36:19 -0800

    Assembling a Navigator

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+	return (
+		<View style={styles.backgroundStyle}>
+			<Feather name="search" size={30} />
+			<TextInput style={styles.inputStyle} placeholder="Search" />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	backgroundStyle: {
+		backgroundColor: '#F0EEEE',
+		height: 50,
+		borderRadius: 5,
+		marginHorizontal: 15,
+		flexDirection: 'row'
+	},
+	inputStyle: {
+		borderColor: 'black',
+		borderWidth: 1,
+		flex: 1
+	}
+});
+
+export default SearchBar;
