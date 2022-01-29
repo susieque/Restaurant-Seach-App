ae538de583a6256ea64229bd75b72393f688c1e6
Susie Riley <susie09rob@gmail.com>
2022-01-28 19:10:36 -0800

    Quick fix

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity
 } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results, navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={results}
-        keyExtractor={result => result.id}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate('ResultsShow', { id: item.id })
-              }
-            >
-              <ResultsDetail result={item} />
-            </TouchableOpacity>
-          );
-        }}
-      />
-    </View>
-  );
+	if (!results.length) {
+		return null;
+	}
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>{title}</Text>
+			<FlatList
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				data={results}
+				keyExtractor={(result) => result.id}
+				renderItem={({ item }) => {
+					return (
+						<TouchableOpacity
+							onPress={() =>
+								navigation.navigate('ResultsShow', { id: item.id })
+							}
+						>
+							<ResultsDetail result={item} />
+						</TouchableOpacity>
+					);
+				}}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginLeft: 15,
-    marginBottom: 5
-  },
-  container: {
-    marginBottom: 10
-  }
+	title: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginLeft: 15,
+		marginBottom: 5
+	},
+	container: {
+		marginBottom: 10
+	}
 });
 
 export default withNavigation(ResultsList);
-
